cbf5e0b77dea49f1a6692c32229774262fe35eac
Rahul Chandra <rahul@projecteclipse.org>
2024-07-16 22:15:09 -0400

    stuff

--- lidar_init.py
+++ lidar_init.py
@@ -1,23 +1,28 @@
-from adafruit_rplidar import RPLidar
 import os
+import sys
+from math import floor
+from adafruit_rplidar import RPLidar
 
 
-PORT = '/dev/ttyUSB0'
-lidar = RPLidar(NONE, PORT)
-# scan_data[angle] = distance
-angle_mount = 45
-scan_data = [0] * 360
-    
+# Setup the RPLidar
+PORT_NAME = '/dev/ttyUSB0'
+lidar = RPLidar(None, PORT_NAME, timeout=3)
 
+# used to scale data to fit on the screen
+max_distance = 0
+
+def process_data(data):
+    print(data)
+
+scan_data = [0]*360
+lidar.set_pwm(255)
 try:
-    print(lidar.info)
+    #print(lidar.info)
     for scan in lidar.iter_scans():
-        for (_, angle, distance) in scan
-        scandata[min(359, floor(angle))] = distance
+        for (_, angle, distance) in scan:
+            scan_data[min([359, floor(angle)])] = distance
+        process_data(scan_data)
 except KeyboardInterrupt:
     print('Stopping.')
 lidar.stop()
 lidar.disconnect()
-
-
-
