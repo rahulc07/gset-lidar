ff4ae70e422620c9f97e559cb8b8d1ef3aecb0b5
AaravDugar123 <25dugara@gmail.com>
2024-07-18 15:52:19 -0400

    fixed

--- convex_hull.py
+++ convex_hull.py
@@ -34,3 +34,26 @@ def plot_convex_hull(hull):
     ax.set_zlabel('z')
 
     plt.show()
+
+
+def plot_convex_hull_and_point_cloud(hull, points):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+
+    # Plot the entire point cloud
+    ax.plot(points.T[0], points.T[1], points.T[2], "bo", alpha=0.5)  # Blue points with some transparency
+
+    # Plot defining corner points of the convex hull
+    ax.plot(hull.points.T[0], hull.points.T[1], hull.points.T[2], "ko")
+
+    # Plot the simplices (lines) of the convex hull
+    for simplex in hull.simplices:
+        simplex = np.append(simplex, simplex[0])  # Cycle back to the first coordinate
+        ax.plot(hull.points[simplex, 0], hull.points[simplex, 1], hull.points[simplex, 2], "r-")
+
+    # Set axis labels
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+
+    plt.show()
