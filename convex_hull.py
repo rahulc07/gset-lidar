3b3b07a160034af0fc0e0cf21104e477889447dd
AaravDugar123 <25dugara@gmail.com>
2024-07-18 15:47:35 -0400

    Fixed suboptimal code

--- convex_hull.py
+++ convex_hull.py
@@ -3,32 +3,34 @@ from time import sleep
 import scipy
 import numpy as np
 import matplotlib.pyplot as plt
+
+
 def convex_hull_volume(three_d_point_cloud):
     hull = scipy.spatial.ConvexHull(three_d_point_cloud)
     volume = hull.volume
     plot_convex_hull(hull)
+
+
 def plot_convex_hull(hull):
-    """
-    Plots a convex hull object.
 
-    Parameters:
-    hull (scipy.spatial.ConvexHull): The convex hull object to plot.
+    points = hull.points
 
-    """
-    if not isinstance(hull, scipy.spatial.ConvexHull):
-        raise ValueError("The input must be a scipy.spatial.ConvexHull object")
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
 
-    plt.figure()
-    plt.plot(hull.points[:, 0], hull.points[:, 1], 'o', markersize=5, label='Points')
-    
+    # Plot defining corner points
+    ax.plot(points.T[0], points.T[1], points.T[2], "ko")
+
+    # 12 = 2 * 6 faces are the simplices (2 simplices per square face)
     for simplex in hull.simplices:
-        plt.plot(hull.points[simplex, 0], hull.points[simplex, 1], 'k-')
+        # Here we cycle back to the first coordinate
+        simplex = np.append(simplex, simplex[0])
+        ax.plot(points[simplex, 0], points[simplex, 1],
+                points[simplex, 2], "r-")
 
-    plt.plot(hull.points[hull.vertices, 0], hull.points[hull.vertices, 1], 'r--', lw=2, label='Convex Hull')
-    plt.fill(hull.points[hull.vertices, 0], hull.points[hull.vertices, 1], 'c', alpha=0.3)
-    
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.title('Convex Hull')
-    plt.legend()
+    # Make axis label
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+
     plt.show()
