9b818214466e7e3f8d0805c3ccaaf299cd67ce27
AaravDugar123 <25dugara@gmail.com>
2024-07-22 22:00:48 -0400

    Find z mind statistcal

--- convert_to_3d_cloud.py
+++ convert_to_3d_cloud.py
@@ -8,43 +8,48 @@ import scipy.stats
 # y is forward backward
 # z is depth
 
+
 def convert_to_3d(point_cloud):
-    #print(point_cloud)
+    # print(point_cloud)
     three_dimensional_point_cloud = []
     for point in point_cloud:
         rotation_angle = point[0]
         distance = point[1]
         # If this math works I am him - Rahul
         lamb_radians = abd.lamb * .0174
-        rotation_angle_rads = rotation_angle *.0174
-        y = float(distance * np.cos(lamb_radians) * np.cos(rotation_angle_rads))
+        rotation_angle_rads = rotation_angle * .0174
+        y = float(distance * np.cos(lamb_radians)
+                  * np.cos(rotation_angle_rads))
         # pi / 2 rads = 1.57
-        x = float(distance * np.cos(lamb_radians)) * np.sin(rotation_angle_rads)
-        
+        x = float(distance * np.cos(lamb_radians)) * \
+            np.sin(rotation_angle_rads)
+
         z = float(distance * np.sin(lamb_radians))
         three_dimensional_point_cloud.append([x, y, z])
-    #print(three_dimensional_point_cloud)
+    # print(three_dimensional_point_cloud)
     z_min = find_z_min(three_dimensional_point_cloud)
-    new_three_dimensional_point_cloud  = []
+    new_three_dimensional_point_cloud = []
     # Get z mean
     z_array = []
     for point in three_dimensional_point_cloud:
         z_array.append(point[2])
-    z_mean = np.median(z_array) 
+    z_mean = np.median(z_array)
     z_std = scipy.stats.iqr(z_array)
 
     for i in range(len(three_dimensional_point_cloud)):
         # Parameter tweak for volume anal
         # print(three_dimensional_point_cloud[i][2])
-        if (three_dimensional_point_cloud[i][2] < z_mean + 2.5 * z_std) and (three_dimensional_point_cloud[i][2] > z_mean - 3 * z_std ):
-        #if (three_dimensional_point_cloud[i][2] > np.percentile(z_array, 25) - 1.5 * z_std ) and (three_dimensional_point_cloud[i][2] < np.percentile(z_array, 75) + 1.5*z_std):
-            #three_dimensional_point_cloud[i][2] -= z_min
-            new_three_dimensional_point_cloud.append(three_dimensional_point_cloud[i])
-    #print(new_three_dimensional_point_cloud)
-    #print(z_mean)
-    #print(z_std)
+        if (three_dimensional_point_cloud[i][2] < z_mean + 2.5 * z_std) and (three_dimensional_point_cloud[i][2] > z_mean - 3 * z_std):
+            # if (three_dimensional_point_cloud[i][2] > np.percentile(z_array, 25) - 1.5 * z_std ) and (three_dimensional_point_cloud[i][2] < np.percentile(z_array, 75) + 1.5*z_std):
+            # three_dimensional_point_cloud[i][2] -= z_min
+            new_three_dimensional_point_cloud.append(
+                three_dimensional_point_cloud[i])
+    # print(new_three_dimensional_point_cloud)
+    # print(z_mean)
+    # print(z_std)
     find_x(new_three_dimensional_point_cloud)
-    return new_three_dimensional_point_cloud 
+    return new_three_dimensional_point_cloud
+
 
 def find_z_min(three_dimensional_point_cloud):
     current_min = 10000
@@ -53,12 +58,12 @@ def find_z_min(three_dimensional_point_cloud):
             current_min = point[2]
     return current_min
 
-def find_x(three_dimensional_point_cloud):
-    z_max_index = 0
-    for point in range(len(three_dimensional_point_cloud)):
-        if three_dimensional_point_cloud[point][2] > three_dimensional_point_cloud[point][2]:
-            z_max_index = point
-    print(three_dimensional_point_cloud[z_max_index][0])
-    return three_dimensional_point_cloud[z_max_index][0]
 
-
+def find_average_x_of_deepest_points(three_dimensional_point_cloud):
+    sorted_point_cloud = sorted(
+        three_dimensional_point_cloud, key=lambda point: point[2])
+    start_index = int(0.75 * len(sorted_point_cloud))
+    deepest_points = sorted_point_cloud[start_index:]
+    total_x = sum(point[0] for point in deepest_points)
+    average_x = total_x / len(deepest_points)
+    print(average_x)
